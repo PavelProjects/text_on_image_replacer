dcf48a63a13036b300cb7d4b4bfb5720b52f9623
poeblo <paveloalexeev52@gmail.com>
2019-11-18 22:59:35 +0300

    Fixed first bugs

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.storage.sync.get("extension_state", function(res){
     if(res["extension_state"]=="on"){
         var input=document.getElementsByClassName("im_editable im-chat-input--text _im_text")[0];
         document.addEventListener('keyup', function(){
-            console.log(input.innerHTML);
+            //console.log(input.innerHTML);
             if(input.innerHTML!=""){
                 chrome.storage.sync.get(input.innerHTML, function(res){
                     var src=res[input.innerHTML];
@@ -19,7 +19,7 @@ chrome.storage.sync.get("extension_state", function(res){
 chrome.storage.onChanged.addListener(function(changes, namespace){
     for(var key in changes){
         var storageChange = changes[key];
-        if(key=="extension_state" && storageChange.newValue=="on"){
+        if(key=="extension_state"){
             window.location.reload();
         }
     }
